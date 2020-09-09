a06fe7eca8a6da6d48bafd851bd8c81a6351bc4b
vinay-coder1 <vinays10998@gmail.com>
2020-09-09 21:34:36 +0530

    encrypted birthday mailIds for birthday schedular

--- src/main/java/com/xworkz/service/MailSchedularServiceImpl.java
+++ src/main/java/com/xworkz/service/MailSchedularServiceImpl.java
@@ -10,9 +10,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -35,7 +37,9 @@ import org.springframework.web.client.RestTemplate;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import com.xworkz.dto.Subscriber;
+import com.xworkz.util.EncryptionHelper;
 import com.xworkz.util.ExcelFileColumn;
+import com.xworkz.util.MailSchedularConstants;
 
 @Service("emailService")
 public class MailSchedularServiceImpl implements MailSchedularService {
@@ -55,70 +59,50 @@ public class MailSchedularServiceImpl implements MailSchedularService {
 	private String excelFilelink;
 	@Value("${imagesJsonlink}")
 	private String imagesJsonlink;
+	@Value("${ccmailID}")
+	private String ccmailID;
+	@Autowired
+	private EncryptionHelper encryptionHelper;
 
 	public void birthadyMailSender() throws URISyntaxException, IOException {
 		logger.info("Invoked birthadyMailSender in service");
 
 		try {
+			@SuppressWarnings("unused")
+			boolean flag = false;
+			Date today = new Date();
 			List<Subscriber> subcriberList = getListOfSubscribersFromExcel();
 
-			for (Subscriber subscriber : subcriberList) {
+			if (Objects.nonNull(subcriberList)) {
+				for (Subscriber subscriber : subcriberList) {
+					logger.info("subsribers=", subscriber.toString());
+					Integer originaldate = (int) ((subscriber.getDob()) - MailSchedularConstants.ExcelCell_Dycription_value);
+					String originalStringDate = originaldate.toString();
+					logger.info("originaldate {}", originalStringDate);
 
-				Integer originaldate = (int) ((subscriber.getDob()) - 599);
-				String originalStringDate = originaldate.toString();
-				logger.info("originaldate {}", originalStringDate);
-				
-				Integer length = originalStringDate.length();	
-				if (length.equals(7)) {
-					DecimalFormat decimalFormat = new DecimalFormat("00000000");
-					String converted = decimalFormat.format(originaldate);
-					originalStringDate = converted;
-					logger.info("updated value= {}",originalStringDate);	
+					originalStringDate = validateDOBLength(originaldate, originalStringDate);
+
+					SimpleDateFormat formatter1 = new SimpleDateFormat(MailSchedularConstants.SimpleDateReadFormat_value);
+					Date date = formatter1.parse(originalStringDate);
+					SimpleDateFormat formatter2 = new SimpleDateFormat(MailSchedularConstants.SimpleDateWriteFormat_value);
+					String formatedTodayDate = formatter2.format(today);
+					logger.info("local date {}", formatedTodayDate);
+
+					String formatedDob = formatter2.format(date);
+					logger.info("custom formated originaldate date {}", formatedDob);
+
+					if (Objects.nonNull(formatedDob) && Objects.nonNull(formatedTodayDate) && (formatedTodayDate).equals(formatedDob)) {
+						extractedAndEmailSending(subscriber, formatedTodayDate, formatedDob);
+						flag = true;
+					}
+
+				}
+				if (flag = false) {
+					logger.debug("No birthday found for today's date {}", today);
 				}
 
-				SimpleDateFormat formatter1 = new SimpleDateFormat("ddMMyyy");
-				Date date = formatter1.parse(originalStringDate);
-				Date today = new Date();
-				SimpleDateFormat formatter2 = new SimpleDateFormat("dd-MM");
-				String formatedTodayDate = formatter2.format(today);
-				logger.info("local date {}", formatedTodayDate);
-				
-				String formatedDob = formatter2.format(date);
-				logger.info("custom formated originaldate date {}", formatedDob);
-
-				if ((formatedTodayDate).equals(formatedDob)) {
-					logger.info("subscriber = {} Macthed DOB = {}", subscriber.getFullName(),(formatedTodayDate).equals(formatedDob));
-					
-					RestTemplate restTemplate = new RestTemplate();
-					HttpHeaders headers = new HttpHeaders();
-					headers.setContentType(MediaType.APPLICATION_JSON);
-					HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-					ResponseEntity<String> responseEntity = restTemplate.exchange(imagesJsonlink, HttpMethod.GET, entity, String.class);
-					String data = responseEntity.getBody();
-
-					JSONObject jsonObject = new JSONObject(data);
-				    JSONArray arrayList = jsonObject.toJSONArray(jsonObject.names());				
-					Random random = new Random();
-					Integer randomInt =  random.nextInt(arrayList.length());
-					Object imageLink = arrayList.get(randomInt);
-					logger.info("birthday image link= {}",imageLink);
-					
-
-					Context context1 = new Context();
-					context1.setVariable("subcriberName", subscriber.getFullName());
-					context1.setVariable("imageLink", imageLink);
-					String content = templateEngine.process("birthdayMailTemplate", context1);
-					MimeMessagePreparator messagePreparator = mimeMessage -> {
-
-						MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
-						messageHelper.setFrom(mailFrom);
-						messageHelper.setTo(subscriber.getEmailId());
-						messageHelper.setSubject(bdayMailSubject);
-						messageHelper.setText(content, true);
-					};
-					emailService.validateAndSendMailByMailId(messagePreparator);
-				}
-
+			} else {
+				logger.debug("subcriberList from getListOfSubscribersFromExcel Is null");
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -127,35 +111,127 @@ public class MailSchedularServiceImpl implements MailSchedularService {
 
 	}
 
+	private String validateDOBLength(Integer originaldate, String originalStringDate) {
+		Integer length = originalStringDate.length();
+		if (Objects.nonNull(length) && Objects.nonNull(originaldate) && length.equals(7)) {
+			DecimalFormat decimalFormat = new DecimalFormat(MailSchedularConstants.DecimalFormat_value);
+			String converted = decimalFormat.format(originaldate);
+			originalStringDate = converted;
+			logger.info("updated value= {}", originalStringDate);
+		} else {
+			logger.debug("originalStringDate length Is null");
+		}
+		return originalStringDate;
+	}
+
+	private void extractedAndEmailSending(Subscriber subscriber, String formatedTodayDate, String formatedDob) {
+		logger.info("subscriber = {} Macthed DOB = {}", subscriber.getFullName(),
+				(formatedTodayDate).equals(formatedDob));
+
+		if (Objects.nonNull(subscriber) && Objects.nonNull(formatedTodayDate) && Objects.nonNull(formatedDob)) {
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+			ResponseEntity<String> responseEntity = restTemplate.exchange(imagesJsonlink, HttpMethod.GET, entity,
+					String.class);
+			if (Objects.nonNull(responseEntity)) {
+				String data = responseEntity.getBody();
+
+				JSONObject jsonObject = new JSONObject(data);
+				JSONArray arrayList = jsonObject.toJSONArray(jsonObject.names());
+				Random random = new Random();
+				Integer randomInt = random.nextInt(arrayList.length());
+				Object imageLink = arrayList.get(randomInt);
+				logger.info("birthday image link= {}", imageLink);
+
+				Context context1 = new Context();
+				context1.setVariable("subcriberName", subscriber.getFullName());
+				context1.setVariable("imageLink", imageLink);
+
+				if (Objects.nonNull(context1)) {
+					String content = templateEngine.process("birthdayMailTemplate", context1);
+					MimeMessagePreparator messagePreparator = mimeMessage -> {
+
+						String mailId = subscriber.getEmailId();
+						if (Objects.nonNull(mailId)) {
+							if (mailId.contains(MailSchedularConstants.Gmail_value_placer)) {
+								mailId = mailId.replace(MailSchedularConstants.Gmail_value_placer, MailSchedularConstants.Gmail_value_Replacer);
+
+								if (mailId.contains(MailSchedularConstants.Outlook_value_placer)) {
+									mailId = mailId.replace(MailSchedularConstants.Outlook_value_placer,MailSchedularConstants.Outlook_value_Replacer);
+
+									if (mailId.contains(MailSchedularConstants.Yahoo_value_placer)) {
+										mailId = mailId.replace(MailSchedularConstants.Yahoo_value_placer, MailSchedularConstants.Yahoo_value_Replacer);
+
+										MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+										messageHelper.setFrom(mailFrom);
+										messageHelper.setCc(ccmailID);
+										messageHelper.setTo(mailId); 
+										messageHelper.setSubject(bdayMailSubject);
+										messageHelper.setText(content, true);
+
+									}
+								}
+							}
+						}
+
+						else {
+							logger.debug("subscriber mail is empty {}", subscriber.getFullName());
+						}
+					};
+					emailService.validateAndSendMailByMailId(messagePreparator);
+				}
+			}
+		} else {
+			logger.debug("got null object in extractedAndEmailSending");
+		}
+	}
+
+	@SuppressWarnings("deprecation")
 	public List<Subscriber> getListOfSubscribersFromExcel() throws IOException {
 		List<Subscriber> subscribersList = new ArrayList<Subscriber>();
 		Workbook workbook = null;
 		ByteArrayInputStream inputStream = null;
 		try {
 			int i = 0;
-			URI url = new URI(excelFilelink);
+			URI url = new URI(encryptionHelper.decrypt(excelFilelink));
 			RestTemplate restTemplate = new RestTemplate();
 			ResponseEntity<byte[]> responseEntity = restTemplate.getForEntity(url, byte[].class);
-			byte[] result = responseEntity.getBody();
+			if (Objects.nonNull(responseEntity)) {
+				byte[] result = responseEntity.getBody();
 
-			logger.info("Staring..........");
-			inputStream = new ByteArrayInputStream(result);
-			workbook = new XSSFWorkbook(inputStream);
-			Sheet excelSheet = workbook.getSheetAt(0);
-			logger.info("Last Row Number of Is Excel file {} ", excelSheet.getLastRowNum());
-			logger.info("Excel file Is opened");
+				logger.info("Staring..........");
+				inputStream = new ByteArrayInputStream(result);
+				if (Objects.nonNull(inputStream)) {
+					workbook = new XSSFWorkbook(inputStream);
+					Sheet excelSheet = workbook.getSheetAt(0);
+					logger.info("Last Row Number of Is Excel file {} ", excelSheet.getLastRowNum());
+					logger.info("Excel file Is opened");
 
-			for (Row row : excelSheet) { // For each Row.
+					if (Objects.nonNull(excelSheet)) {
+						for (Row row : excelSheet) { // For each Row.
 
-				Cell nameCell = row.getCell(ExcelFileColumn.ExcelFile_NAME_CELL);
-				Cell emailCell = row.getCell(ExcelFileColumn.ExcelFile_EMAIL_CELL);
-				Cell dobCell = row.getCell(ExcelFileColumn.ExcelFile_DOB_CELL);
+							Cell nameCell = row.getCell(ExcelFileColumn.ExcelFile_NAME_CELL);
+							nameCell.setCellType(CellType.STRING);
+							Cell emailCell = row.getCell(ExcelFileColumn.ExcelFile_EMAIL_CELL);
+							emailCell.setCellType(CellType.STRING);
+							Cell dobCell = row.getCell(ExcelFileColumn.ExcelFile_DOB_CELL);
+							dobCell.setCellType(CellType.NUMERIC);
 
-				subscribersList.add(new Subscriber(nameCell.getStringCellValue(), emailCell.getStringCellValue(),dobCell.getNumericCellValue()));
-				subscribersList.remove(0);
-				logger.info("No: {} Value: {} Data Is Read and Stored in List", (++i), nameCell.getStringCellValue());
+							if (Objects.nonNull(nameCell) && Objects.nonNull(emailCell) && Objects.nonNull(dobCell)) {
+								subscribersList.add(new Subscriber(nameCell.getStringCellValue(),emailCell.getStringCellValue(), dobCell.getNumericCellValue()));
+								logger.info("No: {} Value: {} Data Is Read and Stored in List", (++i),nameCell.getStringCellValue());
+							} else {
+								logger.debug("excel data is null in getListOfSubscribersFromExcel");
+							}
+						}
+						subscribersList.remove(0);
+					}
+				}
+			} else {
+				logger.debug("responseEntity null in getListOfSubscribersFromExcel");
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 			logger.error("Error is {} and Message is {}", e, e.getMessage());
